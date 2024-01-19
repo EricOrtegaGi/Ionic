d6ffec2c5b70a539c3aa3aedbc698bc16864100c
EricOrtegaGi <ericortega@iesebre.com>
2024-01-19 17:41:22 +0100

    Ionic casi finalitzat, reformatat totalment funcional amb contador per segon

--- provaionic-master/src/views/HomePage.vue
+++ provaionic-master/src/views/HomePage.vue
@@ -3,94 +3,151 @@
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Comptador App</ion-title>
+        <ion-buttons slot="primary">
+          <ion-button color="primary" fill="solid" @click="InfoPopup">
+            <ion-icon :icon="infoIcon"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
+      <ion-header class="ion-no-border ion-padding-top ion-padding-horizontal">
+        <ion-grid>
+          <ion-row>
+            <ion-col>
+              <div class="ion-text-start" id="score">
+                Clicks per Second: {{ cps.toFixed(2) }}
+              </div>
+            </ion-col>
+            <ion-col>
+              <div class="ion-text-end">
+                Time Left: {{ temporitzador }}
+                Counter: {{ comptador }}
+              </div>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-header>
+
       <div id="container">
-        <ion-button @click="handleButtonClick" :disabled="counterDisabled">{{ funciona ? 'Tap Me' : 'Start Game' }}</ion-button>
-        <p>Time: {{ temporitzador }}</p>
-        <p>Counter: {{ comptador }}</p>
-        <p>Clicks per Second: {{ cps.toFixed(2) }}</p>
+        <ion-button @click="Boto" :disabled="counterDisabled">{{ funciona ? 'Tap Me' : 'Start Game' }}</ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { ref, onBeforeUnmount } from 'vue';
-import { IonButton } from '@ionic/vue';
+<script>
+import { alertController, IonButton, IonButtons, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar, toastController } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { informationCircleOutline } from "ionicons/icons";
 
 const INITIAL_TIME = 5;
 
-let comptador = ref(0);
-let temporitzador = ref(INITIAL_TIME);
-let funciona = ref(false);
-let cps = ref(0);
-
-let countdownTimer;
-let clickCountTimer;
-
-const inicia = () => {
-  countdownTimer = setInterval(() => {
-    if (temporitzador.value > 0) {
-      temporitzador.value--;
-      updatecps();
-    } else {
-      endGame();
+export default defineComponent({
+  name: 'ComptadorApp',
+  components: {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonGrid
+  },
+  setup () {
+    return {
+      infoIcon: informationCircleOutline,
+      counterDisabled: false,
     }
-  }, 1000);
-};
+  },
+  data () {
+    return {
+      comptador: 0,
+      temporitzador: INITIAL_TIME,
+      funciona: false,
+      cps: 0
+    }
+  },
+  watch: {
+    temporitzador: function(newTime) {
+      if (newTime <= 0) {
+        this.funciona = false;
+        clearInterval(this.countdownTimer);
+        clearInterval(this.clickCountTimer);
+        this.Resultat();
+        this.comptador = 0;
+      }
+    }
+  },
+  methods: {
+    async InfoPopup() {
+      const alert = await alertController.create({
+        header: 'App Comptador',
+        subHeader: 'Creado por Eric Ortega Gisbert',
+        message: 'Puedes encontrar el código fuente en: <a href="https://github.com/EricOrtegaGi/Ionic">https://github.com/EricOrtegaGi/Ionic</a>',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    },
+    Boto () {
+      if (!this.funciona) {
+        this.startGame();
+      } else {
+        this.increment();
+      }
+    },
+    startGame () {
+      this.init();
+      this.funciona = true;
+      this.countdownTimer = setInterval(() => {
+        if (this.temporitzador > 0) {
+          this.temporitzador--;
+          this.cps();
+        } else {
+          this.endGame();
+        }
+      }, 1000);
+    },
+    increment () {
+      this.comptador++;
+    },
+    endGame () {
+      clearInterval(this.countdownTimer);
+      clearInterval(this.clickCountTimer);
+      this.Resultat();
+      this.reset();
+    },
+    reset () {
+      this.comptador = 0;
+      this.temporitzador = INITIAL_TIME;
+      this.funciona = false;
+      this.cps = 0;
+    },
+    cps () {
+      this.cps = this.comptador / (INITIAL_TIME - this.temporitzador);
+    },
+    init () {
+      this.clickCountTimer = setInterval(() => {
+        this.cps();
+      }, 1000);
+    },
+    async Resultat () {
+      const message = `Game Over! Counter: ${this.comptador}`;
+      console.log(message);
+      // Show toast message
+      const toast = await toastController.create({
+        color: 'dark',
+        duration: 2000,
+        message,
+        showCloseButton: true
+      });
 
-const handleButtonClick = () => {
-  if (!funciona.value) {
-    startGame();
-  } else {
-    incrementar();
+      await toast.present();
+    }
   }
-};
-
-const startGame = () => {
-  inicia();
-  init();
-  funciona.value = true;
-};
-
-const incrementar = () => {
-  comptador.value += 1;
-};
-
-const endGame = () => {
-  clearInterval(countdownTimer);
-  clearInterval(clickCountTimer);
-  // Show toast message
-  const message = `Game Over! Counter: ${comptador.value}`;
-  console.log(message);
-  reset();
-};
-
-const reset = () => {
-  comptador.value = 0;
-  temporitzador.value = INITIAL_TIME;
-  funciona.value = false;
-  cps.value = 0;
-};
-
-const counterDisabled = ref(false);
-
-const updatecps = () => {
-  cps.value = comptador.value / (INITIAL_TIME - temporitzador.value);
-};
-
-const init = () => {
-  clickCountTimer = setInterval(() => {
-    updatecps();
-  }, 1000);
-};
-
-onBeforeUnmount(() => {
-  clearInterval(countdownTimer);
-  clearInterval(clickCountTimer);
 });
 </script>
 
